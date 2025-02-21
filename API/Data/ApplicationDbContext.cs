5ab3e7e9ce161471e62da49375781483a1e566fd
Tbetcha <troy.boettger@gmail.com>
2025-02-20 23:34:41 -0500

    init

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions) { }
+
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
